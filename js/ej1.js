070782144882a77547a57fd34409a7fdb95756dd
matias <pagosptcmatias@gmail.com>
2022-07-27 19:35:49 -0300

    agregue ej1 y 2

--- js/ej1.js
+++ js/ej1.js
@@ -1,2 +1,43 @@
 // 1- Crea un objeto llamado auto que tenga algunas características como el color, marca, modelo y
 //  si está encendido o apagado. Crea los métodos necesarios para permitir encender y apagar el auto.
+
+let auto = {
+  color: "rojo",
+  marca: "ford",
+  modelo: "fiesta",
+  encendido: 1,
+  apagado: 0,
+  quieto() {
+    document.write("el auto esta quieto");
+  },
+  encender(girarEncender) {
+    if (girarEncender === this.encendido) {
+      document.write("<br> el auto se encendio");
+    } else {
+      document.write("<br> el auto no se encendio y sigue quieto");
+    }
+  },
+
+ 
+  apagar(girarApagar) {
+    if (girarApagar <= this.apagado) {
+      document.write("<br> el auto se apago");
+    } else if(girarApagar <= this.encendido){
+      document.write("<br> el auto continua encendido");
+    }else{
+        document.write('<br> el auto nunca se encendio')
+    }
+  },
+};
+auto.quieto(auto);
+
+let girarEncender = parseInt(
+  prompt("ingrese 1 para girar la llave y encender el auto")
+);
+auto.encender(girarEncender);
+
+
+let girarApagar = parseInt(
+  prompt("ingrese 0 para girar la llave y apagar el auto")
+);
+auto.apagar(girarApagar);
--- js/ej2.js
+++ js/ej2.js
@@ -8,3 +8,41 @@
 // Utiliza este objeto para mostrar la descripción, ingresar y extraer dinero y volver a mostrar la descripción del estado de la cuenta.
 
 // Rectángulos
+let cuenta = {
+    titular: "alex",
+    saldo: 0,
+    // transferir() {
+    //   console.log("estamos en el metodo transferir");
+    //   console.log(this);
+    // },
+    ingresar(deposito) {
+      console.log(this);
+      this.saldo += deposito; //objeto.saldo = deposito + objeto.saldo
+      console.log("se ingreso$" + this.saldo);
+    },
+    extraer(retiro) {
+      if (this.saldo >= retiro) {
+        this.saldo -= retiro;
+        document.write(`<p>se retiro ${retiro}</p>`);
+      } else {
+        document.write(`<p>saldo insuficiente</p>`);
+      }
+    },
+    informar (){
+      
+      document.write(
+        `<p>La cuenta de ${this.titular}, posee un saldo de ${this.saldo}</p>`
+      );
+    },
+  };
+  cuenta.informar(cuenta);
+  console.log(this)
+  
+  let deposito = parseFloat(prompt("ingrese el monto que desea ingresar"));
+  cuenta.ingresar(deposito);
+  
+  let retiro = parseFloat(prompt("ingrese el monto a extraer"));
+  cuenta.extraer(retiro);
+  
+  cuenta.informar(cuenta);
+  
